d16c6e468316648688f88e97032513387decf7ea
alexlecornu <al1119@exeter.ac.uk>
2026-02-13 11:57:50 +0000

    completed 1

---
+++ CopyArrayApp.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class CopyArrayApp{
+  public static void main(String[] args) {
+    int nVals = args.length;
+    int[] myVals = new int[nVals];
+    ArrayList<Integer> uniqueVals = new ArrayList<Integer>();
+
+    for (int i = 0; i < nVals; i++) {
+      int item = Integer.parseInt(args[i]);
+      myVals[i] = item;
+      if(uniqueVals.contains(myVals[i]) != true){
+            uniqueVals.add(myVals[i]);
+            }
+        }
+    System.out.println("original values: " + Arrays.toString(myVals));
+    System.out.println("unique values: " + uniqueVals);
+  }
+}
